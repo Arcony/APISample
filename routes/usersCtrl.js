4a9a5f60db6087e09c7652bdf69bec10795bd105
Jean-François <Galietti.jeanfrancois@gmail.com>
2019-01-29 17:34:01 +0100

    5 12:00

--- routes/usersCtrl.js
+++ routes/usersCtrl.js
@@ -1,6 +1,9 @@
 var bcrypt = require('bcryptjs');
 var models = require('../models');
 var jwtUtils = require('../utils/jwt.utils');
+var asyncLib = require('async');
+const EMAIL_REGEX     = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const PASSWORD_REGEX  = /^(?=.*\d).{4,8}$/;
 //routes
 
 module.exports = {
@@ -15,38 +18,66 @@ module.exports = {
       return res.status(400).json({ 'error': 'missing parameters' });
     }
 
-    // TODO verify pseudo length mail regex password etc
-    models.User.findOne({
-      attributes: ['email'],
-      where: { email: email}
-    })
-    .then(function(userFound) {
-      if (!userFound) {
-        bcrypt.hash(password, 5, function( err,bcryptedPassword ) {
-          var newUser = models.User.create({
-            email: email,
-            username: username,
-            password: bcryptedPassword,
-            bio: bio,
-            isAdmin: 0
-          })
-          .then(function(newUser){
-            return res.status(201).json({
-              'userId': newUser.id
-            })
-          })
-          .catch(function(err) {
-            return res.status(500).json({'error': 'cannot add user'})
-          });
-        });
-      } else {
-        return res.status(409).json({'error': 'user already exist'});
-      }
-    })
-    .catch(function(err) {
-      return res.status(500).json({'error': 'unable to verify user'});
-    });
-  },
+    if(username.length >= 13 || username.length <= 4) {
+      return res.status(400).json({ 'error': 'Username Too short/long'});
+    }
+
+    if(!EMAIL_REGEX.test(email)){
+        return res.status(400).json({ 'error': 'Email Wrong'});
+    }
+
+    if(!PASSWORD_REGEX.test(email)){
+        return res.status(400).json({ 'error': 'Password Wrong'});
+    }
+
+    asyncLib.waterfall([
+     function(done) {
+       models.User.findOne({
+         attributes: ['email'],
+         where: { email: email }
+       })
+       .then(function(userFound) {
+         done(null, userFound);
+       })
+       .catch(function(err) {
+         return res.status(500).json({ 'error': 'unable to verify user' });
+       });
+     },
+     function(userFound, done) {
+       if (!userFound) {
+         bcrypt.hash(password, 5, function( err, bcryptedPassword ) {
+           done(null, userFound, bcryptedPassword);
+         });
+       } else {
+         return res.status(409).json({ 'error': 'user already exist' });
+       }
+     },
+     function(userFound, bcryptedPassword, done) {
+       var newUser = models.User.create({
+         email: email,
+         username: username,
+         password: bcryptedPassword,
+         bio: bio,
+         isAdmin: 0
+       })
+       .then(function(newUser) {
+         done(newUser);
+       })
+       .catch(function(err) {
+         return res.status(500).json({ 'error': 'cannot add user' });
+       });
+     }
+   ], function(newUser) {
+     if (newUser) {
+       return res.status(201).json({
+         'userId': newUser.id
+       });
+     } else {
+       return res.status(500).json({ 'error': 'cannot add user' });
+     }
+   });
+ },
+
   login: function(req,res) {
     var email = req.body.email;
     var password = req.body.password;
@@ -78,5 +109,25 @@ module.exports = {
     .catch(function(err) {
       return res.status(500).json({ 'error': 'unable to verify user'});
     });
+  },
+
+  getUserProfile : function(req, res) {
+    var headerAuth = req.headers['authorization'];
+    var userId = jwtUtils.getUserId(headerAuth);
+
+    if(userId < 0)
+      return res.status(400).json({'error': 'wrong token'});
+    models.User.findOne({
+      attributes: [ 'id', 'email', 'username', 'bio' ],
+      where: {id : userId}
+    }).then(function(user) {
+      if (user) {
+        res.status(201).json(user);
+      } else {
+        res.status(404).json({'error': 'user not found'});
+      }
+    }).catch(function(err) {
+      res.status(500).json({'error': 'cannot fetch user'});
+    })
   }
 }
